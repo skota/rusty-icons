a6c0de3146f39a97075227a9b20be9b21c7d4ac2
skota <sriramkota@yahoo.com>
2018-02-26 20:26:59 -0500

    start new module resizer to house functions for resizing images

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,12 @@ extern crate image;
 
 // extern crate libby;
 use std::env;
-use std::fs::File;
-use image::{FilterType, GenericImage, Pixel};
+use std::process;
 
 pub mod libby;
+pub mod resize;
 
 fn main() {
-    //ensure programs has atleast some params ..else show mesg and exit
-
-    //check images passed in have the right sizes
-
     // show progress on command line while images are being created?
 
     // aditional functionality - create thumbnails
@@ -26,13 +22,39 @@ fn main() {
     // let thumbnail = img2.resize(200,200, FilterType::Lanczos3);
     // let mut thumper = File::create("resized.png").unwrap();
     // thumbnail.save(&mut thumper, image::PNG).expect("Saving image failed");
-    //resizer splash android /path/to/img
-    //command task os path
 
     let args: Vec<String> = env::args().collect();
-    let arg_struct = libby::libby::parse_args(&args);
+    
+    let config = libby::libby::Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem passing arguments: {}", err);
+        process::exit(1);
+    });
 
-     println!("Argument 1 is: {}", arg_struct.task_name);
-     println!("Argument 2 is: {}", arg_struct.os_name);
-     println!("Argument 3 is: {}", arg_struct.image_name);
+    if config.task_name == "splashes" {
+        //do splashes
+        create_splashes(config)
+    } else {
+        //do icons
+        create_icons(config)
+    }
+}
+
+fn create_splashes(config: libby::libby::Config) {
+    println!("Argument 1 is: {}", config.task_name);
+     println!("Argument 2 is: {}", config.os_name);
+//     println!("Argument 3 is: {}", config.image_name);
+}
+
+fn create_icons(config: libby::libby::Config) {
+    println!("Argument 1 is: {}", config.task_name);
+     println!("Argument 2 is: {}", config.os_name);
+     println!("Argument 3 is: {}", config.image_name);
+    //if os is IOS
+    if config.os_name == "ios" {
+        //validate image ( size, extention - only jpeg, png)
+    } else {
+        //android
+    }    
+
+    //if os is android
 }
---
+++ src/resize.rs
@@ -0,0 +1,10 @@
+use image::{FilterType, GenericImage, Pixel};
+use std::fs::File;
+
+pub mod resize {
+    pub fn resize(args: &[String]) -> Result<(), &'static str> {
+        // check if enough params are passed
+        Ok(())
+    }
+ 
+}
